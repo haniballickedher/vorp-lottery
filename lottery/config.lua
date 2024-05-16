847da371d5b0cea842378cb55cf07bdc98c5aab4
haniballickedher <77361534+haniballickedher@users.noreply.github.com>
2024-05-16 10:59:01 -0800

    Update config.lua

--- lottery/config.lua
+++ lottery/config.lua
@@ -1,1 +1,87 @@
+Config = {}
+   
+    
+--This not yet functional either
+Config.LotteryOfficials={
+   {'admin'}
+}
+--Not yet working
+Config.Webhooks={
+    URL ='https://discord.com/api/webhooks/1193830818934702171/zbapq4SRSDx7bl7cwGrw-EbWFNDkNWIqa7ia3Qg0MMXtuWxt4XVpHlqRomZbsik9_nJj',
+    Color = '16711680',
+    WebhookName = 'Lottery Bot',
+    WebhookLogo = 'https://cdn.discordapp.com/attachments/1180371753495711875/1180392613656928286/icon.png?ex=65ab65c9&is=6598f0c9&hm=152687723ecf73d56cca7af916f52729ef16287075742b74ae2afc32ecdd1372&'
+}
+
+
+
+
+Config.LotteryLocations = { -- https://filmcrz.github.io/blips/
+    {
+        name = 'Stawberry Lottery',
+        city='Strawberry',
+        region='West Elizabeth',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = -1767.5093994140625, y = -381.3931884765625, z = 157.83193969726565},
+    },
+    {
+        name = 'Blackwater Lottery',
+        city='Blackwater',
+        region='West Elizabeth',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = -814.8077392578125, y = -1277.358154296875, z = 43.73770523071289},
+    },
+    {
+        name = 'Armadillo Lottery',
+        city='Armadillo',
+        region='New Austin',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = -3662.2265625, y = -2624.84521484375, z = -13.48766803741455},
+    },
+    {
+        name = 'Tumbleweed Lottery',
+        city='Tumbleweed',
+        region='New Austin',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = -5487.060546875, y = -2939.2705078125, z = -0.28708344697952},
+    },
+    {
+        name = 'Valentine Lottery',
+        city='Valentine',
+        region='New Hanover',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = -172.77517700195312, y = 634.057373046875, z = 114.18964385986328},
+    },
+    {
+        name = 'Rhodes Lottery',
+        city='Rhodes',
+        region='Lemoyne',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = 1289.6468505859375, y = -1300.8714599609375, z = 77.14014434814453},
+    },
+    {
+        name = 'Saint Denis Lottery',
+        city='St Denis',
+        region='Lemoyne',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = 2637.13, y = -1291.36, z = 52.2},
+    },
+    {
+        name = 'Annesburg Lottery',
+        city='Annesburg',
+        region='New Hanover',
+        hash = -44057202,
+        scale = 0.5,
+        coords = {x = 2930.1513671875, y = 1279.6221923828125, z = 44.75285339355469},
+    }
+
+}
+
 
